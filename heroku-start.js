450801b280a922f7a6075e11967c2a2fba5e695f
gustavofmr <gustavo_fernandes@get.inatel.br>
2021-05-27 20:38:16 -0300

    usando npx para chamar heroku-start

--- heroku-start.js
+++ heroku-start.js
@@ -1,5 +1,7 @@
+console.log("Teste1")
 const port = process.env.PORT
 var exec = require('child_process').exec;
 exec('nx serve speedtest-app -p ' + port, function callback(error, stdout, stderr){
+    console.log("Teste2")
 });
 
